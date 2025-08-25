2cf369dd010bb11b47b8ac5f5c770a5b9751d6fd
zorothecoder00 <awledouamoussou95@gmail.com>
2025-08-25 09:28:18 +0000

    Création d'un wrapper SessionWrapper dans src/components qui importe SessionProvider et utilisation de SessionWrapper dans le layout global

---
+++ src/components/SessionWrapper.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { SessionProvider } from 'next-auth/react'
+import type { Session } from 'next-auth'
+
+export default function SessionWrapper({ children, session }: { 
+	children: React.ReactNode 
+	session?: Session | null 
+}) {
+  return <SessionProvider session={session}>{children}</SessionProvider>
+}
