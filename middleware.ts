da7f335a8310931f1379cee37c0ca271da10581e
zorothecoder00 <awledouamoussou95@gmail.com>
2025-09-12 18:05:28 +0000

    Suppression de la fonction getMaintenance dans le middleware qui ralentissait l'appli

--- middleware.ts
+++ middleware.ts
@@ -1,46 +1,45 @@
-import { NextRequest , NextResponse } from 'next/server'
-import { getToken } from "next-auth/jwt"
-import { Role } from '@prisma/client'  
+import { NextRequest, NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+import { Role } from '@prisma/client'
 
 export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })
+  const url = request.nextUrl
+  const { pathname } = url
 
-  	// 🔑 2. Gestion des tokens NextAuth
-	const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET})        
-	// Si l'utilisateur n'est pas connecté → rediriger vers /login
-	if(!token){
-		if(!pathname.startsWith("/auth")){
-			return NextResponse.redirect(new URL("/auth/login", request.url))
-		}
-		return NextResponse.next()
-	}
+  // Si l'utilisateur n'est pas connecté → rediriger vers /login
+  if (!token) {
+    if (!pathname.startsWith('/auth')) {
+      return NextResponse.redirect(new URL('/auth/login', request.url))
+    }
+    return NextResponse.next()
+  }
 
-	// 🎭 3. Vérifie les rôles pour le dashboard
-	const role = token?.role as Role | undefined
+  const role = token?.role as Role | undefined
 
-	// Vérifie accès dashboard
-	if (pathname.startsWith('/dashboard')) {
-	    if (!role) {
-	     return NextResponse.redirect(new URL('/chooseRole', request.url))
-	    }
+  // Vérifie accès dashboard
+  if (pathname.startsWith('/dashboard')) {
+    if (!role) {
+      return NextResponse.redirect(new URL('/chooseRole', request.url))
+    }
 
-	    // Map typée Role → chemin dashboard
-	    const dashboardPaths: Partial<Record<Role, string>> = {
-	      ADMIN: '/dashboard/admin',
-	      ACHETEUR: '/dashboard/acheteur',
-	      VENDEUR: '/dashboard/vendeur',
-	      AGENT: '/dashboard/agent',
-	      ENTREPRISE: '/dashboard/entreprise',
-	    }
+    // Map typée Role → chemin dashboard
+    const dashboardPaths: Partial<Record<Role, string>> = {
+      ADMIN: '/dashboard/admin',
+      ACHETEUR: '/dashboard/acheteur',
+      VENDEUR: '/dashboard/vendeur',
+      AGENT: '/dashboard/agent',
+      ENTREPRISE: '/dashboard/entreprise',
+    }
 
-	    if (pathname !== dashboardPaths[role]) {
-	      return NextResponse.redirect(new URL('/unauthorized', request.url))
-	    }
-	}
+    if (pathname !== dashboardPaths[role]) {
+      return NextResponse.redirect(new URL('/unauthorized', request.url))
+    }
+  }
 
-	return NextResponse.next()
-} 
+  return NextResponse.next()
+}
 
 export const config = {
-	  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ['/dashboard/:path*'],
 }
