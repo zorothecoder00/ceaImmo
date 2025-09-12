ba912324580d3201ee94eb16fa7fc76eb5733a25
zorothecoder00 <awledouamoussou95@gmail.com>
2025-09-12 17:39:04 +0000

    Suppression de getMaintenance dans middleware.ts qui ralentissait tout le site

--- middleware.ts
+++ middleware.ts
@@ -1,40 +1,9 @@
 import { NextRequest , NextResponse } from 'next/server'
 import { getToken } from "next-auth/jwt"
 import { Role } from '@prisma/client'  
-import { prisma } from "@/lib/prisma"
-
-// Petit cache mémoire pour la config
-let cachedConfig: { maintenance: boolean } | null = null
-let lastFetch = 0
-const CACHE_DURATION = 60 * 1000 // 1 minute
-
-async function getConfig(request: NextRequest) {
-  const now = Date.now()
-  if (!cachedConfig || now - lastFetch > CACHE_DURATION) {
-    const res = await fetch(new URL("/api/config/maintenance", request.url).toString())
-    const data = await res.json()
-    cachedConfig = data
-    lastFetch = now
-  }
-  return cachedConfig
-}    
 
 export async function middleware(request: NextRequest) {
-
-	const url = request.nextUrl
-	const { pathname } = url  
-
-	// 🚧 1. Gestion du mode maintenance
-  	const config = await getConfig(request)
-	if (config?.maintenance) {
-	   // exceptions : page maintenance, assets statiques, login admin
-	   const allowedPaths = ["/maintenance", "/auth/login", "/auth/logout", "/api/auth", "/dashboard/admin"]
-	   const isAllowed = allowedPaths.some(p => pathname.startsWith(p))
-
-	   if (!isAllowed) {
-	     return NextResponse.redirect(new URL("/maintenance", request.url))
-	   }
-	  }
+  const { pathname } = request.nextUrl;
 
   	// 🔑 2. Gestion des tokens NextAuth
 	const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET})        
